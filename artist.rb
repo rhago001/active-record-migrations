1a5956c5828667c7cafbc1e659c1f5492230bfbe
Rahel_Hagos <hagos.rahelh@gmail.com>
2020-11-29 23:22:29 -0500

    Done.

---
+++ artist.rb
@@ -0,0 +1,24 @@
+class Artist < ActiveRecord::Base
+
+
+  # @@all =[]
+  # def initialize (name, genre, age, hometown )
+  #   @name =name
+  #   @genre =genre
+  #   @age =age
+  #   @hometown =hometown
+  #   @@all << self
+  #   save
+  # end
+
+  # def self.all
+  #   @@all
+  # end
+
+  # def save 
+
+  # end
+   
+
+
+end
